c884f48c7a779a03ac98c2ed57e16524d1e722ec
Vladimir Pavković <vladimirpavk@gmail.com>
2020-01-22 09:16:30 +0100

    +

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Transition } from 'react-transition-group';
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -9,7 +10,8 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      modalIsOpened: false
+      modalIsOpened: false,
+      blockIsOpened: false
     }
   }
 
@@ -26,9 +28,44 @@ class App extends Component {
   }
 
   render() {
-    return (
+    const defaultStyle = {
+      backgroundColor: 'red',
+      width: 100,
+      height: 100,
+      margin: 'auto',
+      transition: 'opacity 1s ease-out'
+    };
+    const transitionStyles = {
+      entering: { opacity: 0.5 },
+      entered:  { opacity: 1 },
+      exiting:  { opacity: 0.5 },
+      exited:  { opacity: 0 },
+    };
+
+    return (    
       <div className="App">
         <h1>React Animations</h1>
+        <button className="Button" onClick={()=>this.setState((oldState)=>({blockIsOpened: !oldState.blockIsOpened}))}>Toggle block</button>
+       {/*  { this.state.blockIsOpened ? <div style={{
+                                          backgroundColor: 'red',
+                                          width: 100,
+                                          height: 100,
+                                          margin: 'auto'
+                                        }}></div> : null } */}
+        <Transition
+          in={this.state.blockIsOpened}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+        >
+          { state => (
+            <div style={{
+              ...defaultStyle,
+              ...transitionStyles[state]
+            }}></div>
+          )}
+        </Transition>     
+
         <Modal show={this.state.modalIsOpened} closed={(event)=>{this.dismissButtonClicked()}}/>
         <Backdrop show={this.state.modalIsOpened}/>
         <button className="Button" onClick={(event)=>this.buttonClicked()}>Open Modal</button>
